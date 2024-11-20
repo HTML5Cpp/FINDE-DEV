d42e6dd63b518a6057411d9075c58d599e37b323
Octavio Saucedo <octaviosz2003@hotmail.com>
2024-11-20 14:58:03 -0600

    commit 06

--- JS/categorias-color.js
+++ JS/categorias-color.js
@@ -24,20 +24,20 @@ function backgroundCtaegories(pos){
     let categoria_header = document.getElementById('header-category-container-inner');
     let circle_header = document.getElementById('header-category-container-inner-circle');
     let imagesArrayHead = [
-        '../RECURSOS/HEADER_CATEGORIA/head-all.jpeg', 
-        '../RECURSOS/HEADER_CATEGORIA/head-restaurant.jpeg', 
-        '../RECURSOS/HEADER_CATEGORIA/head-culture.jpeg', 
-        '../RECURSOS/HEADER_CATEGORIA/head-fun.jpeg', 
-        '../RECURSOS/HEADER_CATEGORIA/head-party02.jpeg', 
-        '../RECURSOS/HEADER_CATEGORIA/head-health.jpeg'
+        './RECURSOS/HEADER_CATEGORIA/head-all.jpeg', 
+        './RECURSOS/HEADER_CATEGORIA/head-restaurant.jpeg', 
+        './RECURSOS/HEADER_CATEGORIA/head-culture.jpeg', 
+        './RECURSOS/HEADER_CATEGORIA/head-fun.jpeg', 
+        './RECURSOS/HEADER_CATEGORIA/head-party02.jpeg', 
+        './RECURSOS/HEADER_CATEGORIA/head-health.jpeg'
     ];
     let imagesArrayCir = [
-        '../RECURSOS/HEADER_CATEGORIA/circle-all.jpeg', 
-        '../RECURSOS/HEADER_CATEGORIA/circle-restaurant02.jpeg', 
-        '../RECURSOS/HEADER_CATEGORIA/circle-culture.jpeg', 
-        '../RECURSOS/HEADER_CATEGORIA/circle-fun.jpeg', 
-        '../RECURSOS/HEADER_CATEGORIA/circle-party.jpeg', 
-        '../RECURSOS/HEADER_CATEGORIA/circle-health.jpeg'
+        './RECURSOS/HEADER_CATEGORIA/circle-all.jpeg', 
+        './RECURSOS/HEADER_CATEGORIA/circle-restaurant02.jpeg', 
+        './RECURSOS/HEADER_CATEGORIA/circle-culture.jpeg', 
+        './RECURSOS/HEADER_CATEGORIA/circle-fun.jpeg', 
+        './RECURSOS/HEADER_CATEGORIA/circle-party.jpeg', 
+        './RECURSOS/HEADER_CATEGORIA/circle-health.jpeg'
     ];
     categoria_header.style.backgroundImage = `url(${imagesArrayHead[pos]})`;
     circle_header.style.backgroundImage = `url(${imagesArrayCir[pos]})`;
